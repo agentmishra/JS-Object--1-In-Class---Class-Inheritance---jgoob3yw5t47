140a775f2638d4031fed9a7ffc6318ff091848e0
Amit Kumar Mishra <32227450+agentmishra@users.noreply.github.com>
2021-12-27 19:43:48 +0530

    Update shape.js

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,14 @@
 // declare class
+class Shape {
+     constructor() {
+          this.color = "green";
+     }
+     drawShape() {
 
+     }
+     calculateArea() {
+
+     }
+}
 // export class using module.exports
+module.exports = Shape;
