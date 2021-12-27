97557c7c85c9d6eb9450454801ea7aef4cf8c90e
Amit Kumar Mishra <32227450+agentmishra@users.noreply.github.com>
2021-12-27 19:42:43 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,14 @@
-// import using require
-
 // declare class
+class Shape {
+     constructor() {
+          this.color = "green";
+     }
+     drawShape() {
 
+     }
+     calculateArea() {
 
+     }
+}
 // export class using module.exports
+module.exports = Shape;
